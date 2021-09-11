7e94685d5ce83436c7c9ac3bae2f9f885826a32d
nicolealdurien <nicole.aldurien@gmail.com>
2021-09-11 00:04:54 -0400

    starting php

---
+++ PHP-Codecademy/01_intro_to_php.php
@@ -0,0 +1,17 @@
+# Laravel is a PHP framework.
+
+# PHP is used to bulid dynamic web pages. 
+# It can be used directly in-line within HTML.
+# The start of inline PHP is denoted with <?php and ends with ?>
+# echo is used to output text between double quotes.
+# An instruction written in PHP is called a statement.
+# A semicolon is required at the end of each PHP statement.
+
+# PHP can also be executed from the terminal. 
+# When writing PHP scripts, the beginning tag <?php is still required,
+# but the closing tag is no longer required, and is usuallly 
+# left out by convention.
+
+# PHP generally ignores whitespace. It also is not case-sensitive.
+# Comments in PHP can use either // or # to create a single-line comment.
+# Multi-line uses /* */
---
+++ PHP-Codecademy/02_learn_php_variables.php
@@ -0,0 +1,17 @@
+# STRINGS
+# https://www.php.net/manual/en/language.types.string.php
+# String literals can be specified with either single or double quotes,
+# or with heredoc or nowdoc syntax.
+ 
+# ESCAPE SEQUENCES
+# Use backslashes to escape double-quotes or newlines.
+
+# CONCATENATION
+# The concatenation operator in PHP is a period (.)
+# echo "one" . "two" will print onetwo
+
+# VARIABLES
+# Variables can be declared and have values assigned at the same time.
+# To declare a variable, precede it with a dollar sign.
+# This is known as a sigil.
+#
