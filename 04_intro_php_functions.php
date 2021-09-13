6a3aa1f6f3d86a3bd344a3cf9920006342121104
nicolealdurien <nicole.aldurien@gmail.com>
2021-09-13 10:43:08 -0400

    sunday studies

---
+++ 04_intro_php_functions.php
@@ -0,0 +1,13 @@
+# INTRO TO PHP FUNCTIONS
+# PHP uses snake case for variable names BUT camel case for function names.
+# Functions are defined and invoked the same way as in (pre-ES6) JS.
+# function greet() {
+#   echo "Sup!";
+# }
+# greet();
+#
+# RETURN STATEMENTS - same as JS.
+# Any function without a return statement returns NULL.
+# If you try to use NULL in mathematical operations, it functions as 0.
+#
+# PARAMETERS - same as JS.
