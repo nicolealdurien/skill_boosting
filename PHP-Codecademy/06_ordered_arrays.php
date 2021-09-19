7f8b6cf26cb0892f2fe784bf46dafa3f2a228cd8
nicolealdurien <nicole.aldurien@gmail.com>
2021-09-19 00:06:19 -0400

    printing arrays

---
+++ PHP-Codecademy/06_ordered_arrays.php
@@ -0,0 +1,20 @@
+# ORDERED ARRAYS
+# The array() function creates an array of the arguments passed
+# to it, in the order they were passed in.
+#
+# PHP arrays can contain mixed data types in a single array.
+#
+# The built-in function count() will return the number of 
+# elements in an array.
+#
+# We can also skip the array function and just use square brackets.
+# This is known as 'short array syntax'.
+#
+# PRINTING ARRAYS
+# To print an array, we must use print_r() instead of echo. This
+# prints in a human-readable format.
+# If we just want to print the elements, in the array, we can
+# convert the array into a string using the implode() function. This
+# takes two arguments:
+# - a string to use between each element (the "glue"), usually ", "
+# - the array to be joined together (the "pieces")
